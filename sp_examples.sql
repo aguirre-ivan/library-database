4f1a676de77eb378fa25c3bbcfcbf8fece3dd26d
aguirre-ivan <aguirre.ivang@gmail.com>
2022-05-19 05:10:10 -0300

    Added new examples:
    - SP update_book_status
    - SP log_into_log_loan_table

--- sp_examples.sql
+++ sp_examples.sql
@@ -21,3 +21,15 @@ CALL delete_book_category(1, 1);
 CALL delete_book_category(2, 2);
 
 SELECT * FROM book_category; -- to see changes
+
+-- update_book_status examples
+
+CALL update_book_status(1, 3);
+
+SELECT * FROM book_inventory WHERE id_book_inventory = 1; -- to see changes
+
+-- log_into_log_loan_table examples
+
+CALL log_into_log_loan_table(1, 3);
+
+SELECT * FROM log_loan; -- to see changes
